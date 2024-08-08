1cda06d9c2c8ca885fd4ba37e675f1fcdbcab239
knyazkin maxim <knyazkinmaxim626@gmail.com>
2024-08-08 13:30:30 +0300

    Task 1.base-concepts DONE

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,31 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+
+  let d = b ** 2 - 4 * a * c;
+
+  if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1, x2);
+  } else if (d === 0) {
+    let x = -b / (2 * a);
+    arr.push(x);
+  } else {
+    console.log("Корней нет");
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  const monthPercent = percent / 100 / 12;
+  const creditBody = amount - contribution;
+  const monthPayment =
+    creditBody *
+    (monthPercent + monthPercent / ((1 + monthPercent) ** countMonths - 1));
+  let totalPayment = monthPayment * countMonths;
+  totalPayment = +totalPayment.toFixed(2);
+
+  return totalPayment;
 }
