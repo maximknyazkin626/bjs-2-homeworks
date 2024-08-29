7a4d77143ea92b1f5cb89f9d633655af0eb95644
knyazkin maxim <knyazkin.maxim.626@gmail.com>
2024-08-29 20:17:51 +0300

    Почти done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -16,18 +16,25 @@ Student.prototype.setSubject = function (subjectName) {
 
 Student.prototype.addMarks = function (...marks) {
   if (this.hasOwnProperty('marks')) {
-    this.marks = marks;
+    this.marks.push(...marks);
   }
 }
 
 Student.prototype.getAverage = function () {
-  if (this.hasOwnProperty('marks') && this.marks != []) {
-    return Math.sum(this.marks) / this.marks.length;
+  if (this.marks.length !== 0 && this.hasOwnProperty('marks')) {
+    let sum = 0;
+    for (let i = 0; i < this.marks.length; i++) {
+      sum += this.marks[i];
+    }
+    return sum / this.marks.length;
   } else {
     return 0;
   }
+
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
