4822b5ec4715e0a9497005034ade03d6f32f5df9
knyazkin maxim <knyazkinmaxim626@gmail.com>
2024-09-02 09:01:43 +0300

    Task 4 Objects DONE!

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -5,23 +5,21 @@ function Student(name, gender, age) {
   this.marks = [];
 }
 
-let ivan = new Student('Иван','мужской', 22)
-let artem = new Student('Артём', 'мужской', 23);
-
-
+let ivan = new Student("Иван", "мужской", 22);
+let artem = new Student("Артём", "мужской", 23);
 
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
-}
+};
 
 Student.prototype.addMarks = function (...marks) {
-  if (this.hasOwnProperty('marks')) {
+  if (this.hasOwnProperty("marks")) {
     this.marks.push(...marks);
   }
-}
+};
 
 Student.prototype.getAverage = function () {
-  if (this.marks.length !== 0 && this.hasOwnProperty('marks')) {
+  if (this.hasOwnProperty("marks") && this.marks.length !== 0) {
     let sum = 0;
     for (let i = 0; i < this.marks.length; i++) {
       sum += this.marks[i];
@@ -30,11 +28,10 @@ Student.prototype.getAverage = function () {
   } else {
     return 0;
   }
-
-}
+};
 
 Student.prototype.exclude = function (reason) {
   delete this.subject;
   delete this.marks;
   this.excluded = reason;
-}
+};
